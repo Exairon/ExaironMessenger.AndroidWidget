982657a14b81400211c6af11fa20452431c83c2a
Enes Toprak <etoprak174@gmail.com>
2023-01-26 15:23:16 +0300

    add current location selection

--- widget/src/main/java/com/exairon/widget/view/MapsActivity.kt
+++ widget/src/main/java/com/exairon/widget/view/MapsActivity.kt
@@ -1,10 +1,13 @@
 package com.exairon.widget.view
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -12,22 +15,28 @@ import androidx.core.content.ContextCompat
 import com.exairon.widget.R
 import com.exairon.widget.StateManager
 import com.exairon.widget.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.activity_chat.send_button
+import kotlinx.android.synthetic.main.activity_maps.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMapsBinding
     private lateinit var mGoogleMap: GoogleMap
-    private var currentLocation: LatLng? = null
-    private var LOCATION_REQUEST_CODE = 400
+    private var selectedLocation: LatLng? = null
     private var latitude: Double? = null
     private var longitude: Double? = null
+    private var LOCATION_REQUEST_CODE = 400
+
+    private var CURRENT_LOCATION_REQUEST_CODE = 500
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +49,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         send_button.setOnClickListener {
-            if (currentLocation != null) {
-                StateManager.location = currentLocation
+            if (selectedLocation != null) {
+                StateManager.location = selectedLocation
                 val intent = Intent(this, ChatActivity::class.java)
                 startActivity(intent)
             }
         }
+
+        current_location.setOnClickListener {
+            getCurrentLocation()
+        }
     }
 
-    private fun setCurrentLocation() {
+    private fun getCurrentLocation(){
+        if(checkPermissions()){
+            if(isLocationEnabled()) {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermissions()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task ->
+                    val location: Location? = task.result
+                    if(location == null) {
+                        //location null
+                    } else {
+                        selectedLocation = LatLng(location.latitude, location.longitude)
+                        mGoogleMap.addMarker(MarkerOptions().position(selectedLocation!!).title("Current Location"))
+                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedLocation!!, 16.0f))
+                    }
+                }
+            } else {
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION), CURRENT_LOCATION_REQUEST_CODE)
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        if(ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun setSelectedLocation() {
         if (latitude !== null && longitude != null) {
             val location = LatLng(latitude!!, longitude!!)
             mGoogleMap.addMarker(MarkerOptions().position(location).title("Selected Location"))
@@ -63,19 +130,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-
         mGoogleMap = googleMap;
 
         if (intent.getStringExtra("latitude") != null) {
             latitude = intent.getStringExtra("latitude")?.toDouble()
             longitude = intent.getStringExtra("longitude")?.toDouble()
-            setCurrentLocation()
+            setSelectedLocation()
         }
 
         mGoogleMap.setOnMapClickListener { param1 ->
             val location = LatLng(param1.latitude, param1.longitude)
             mGoogleMap.clear()
-            currentLocation = location
+            selectedLocation = location
             mGoogleMap.addMarker(MarkerOptions().position(location).title("Selected Location"))
             mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
         }
@@ -84,22 +150,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun checkPermission() {
         if (ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            setCurrentLocation()
+            setSelectedLocation()
         } else {
             requestPermissions()
         }
     }
 
     private fun requestPermissions() {
-        ActivityCompat.requestPermissions(this, arrayOf("Manifest.permission.ACCESS_FINE_LOCATION"),LOCATION_REQUEST_CODE)
+        ActivityCompat.requestPermissions(this, arrayOf("Manifest.permission.ACCESS_FINE_LOCATION"), LOCATION_REQUEST_CODE)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode == LOCATION_REQUEST_CODE) {
             if (permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION ) {
-                setCurrentLocation()
+                setSelectedLocation()
             }
         }
+        if(requestCode == CURRENT_LOCATION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
+                getCurrentLocation()
+            } else {
+                // Denied
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        val chatIntent = Intent(this, ChatActivity::class.java)
+        startActivity(chatIntent)
     }
 }
